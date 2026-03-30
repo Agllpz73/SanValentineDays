2bcf33a0b97c27ded4aff6f953a91a0c46ffbc8b
Agllpz73 <lopezdiaz1bm@gmail.com>
2026-03-29 18:08:36 -0600

    cambios

--- codigo_QR.py
+++ codigo_QR.py
@@ -1,12 +1,12 @@
 import qrcode
 
 # URL que quieres abrir con el QR
-url = "https://agllpz73.github.io/SanValentineDays/"
+url = "https://forms.gle/aU2JQ5DkoFE19RMV8"
 
 # Crear QR
 img = qrcode.make(url)
 
 # Guardar imagen
-img.save("mi_qr.png")
+img.save("encuestas.png")
 
-print("QR creado como mi_qr.png")
+print("QR creado como encuestas.png")
